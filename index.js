acee4e34f9a04e2f10c302c758f25140b51018b7
bakhler <thehenridit@gmail.com>
2022-11-09 08:12:54 +0200

    Add <,>,&&,||

---
+++ index.js
@@ -0,0 +1,53 @@
+// let condition = true;
+// console.log(condition);
+
+// let number1 = 12;
+// let number2 = 5;
+
+// console.log(number1, ">", number2, "=", number1 > number2);
+// console.log(number1, ">=", number2, "=", number1 >= number2);
+// console.log(number1, "<", number2, "=", number1 < number2);
+// console.log(number1, "<=", number2, "=", number1 <= number2);
+
+// let sting1 = "D";// 68
+// let sting2 = "d"; //100
+
+// console.log(sting1, ">", sting2, "=", sting1 > sting2);
+
+// let sting3 = "d";// 100
+// let sting4 = "D"; //68
+
+// console.log(sting3, ">", sting4, "=", sting3 > sting4);
+
+// let sting5 = "Dom";// 68+111+109 =288
+// let sting6 = "dog"; //100+111+109 =320
+
+// console.log(sting5, ">", sting6, "=", sting5 > sting6);
+
+// let sting7 = "101";//
+// let sting8 = "11"; //False, beacuse 49,4849<49,49
+
+// console.log(sting7, ">", sting8, "=", sting7 > sting8);
+
+// // logic *  '&&'
+// console.log(true, '&&', true, '=', true && true)
+// console.log(false, '&&', true, '=', false && true)
+// console.log(true, '&&', false, '=', true && false)
+// console.log(false, '&&', false, '=', false && false)
+// //logic + '||'
+
+// console.log(true, '||', true, '=', true || true)
+// console.log(false, '||', true, '=', false || true)
+// console.log(true, '||', false, '=', true || false)
+// console.log(false, '||', false, '=', false || false)
+
+let string = "";
+let number = 12;
+let userName = "Brad";
+let bool = false;
+let object = null;
+let resultRemainder = number % 5; //2
+
+let result = string && number;
+console.log(string && number);
+console.log(result);
